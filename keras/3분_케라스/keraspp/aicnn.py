c8c828f7a6346ae1459d997491f095c1aa46ffe1
kkkjerry <jaeyeol2931@gmail.com>
2018-09-28 23:50:47 +0900

    Update

---
+++ keras/3분_케라스/keraspp/aicnn.py
@@ -0,0 +1,199 @@
+
+from sklearn import model_selection, metrics
+from sklearn.preprocessing import MinMaxScaler
+
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+from keras import backend as K
+from keras.utils import np_utils
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# Plot function
+import skeras
+
+# Saving result
+import sfile
+
+class CNN(Model):
+    def __init__(model, nb_classes, in_shape=None):
+        model.nb_classes = nb_classes
+        model.in_shape = in_shape
+        model.build_model()
+        #super().__init__(model.x, model.y)
+        model.compile()
+        
+    def build_model(model):
+        nb_classes = model.nb_classes
+        in_shape = model.in_shape
+
+        x = Input(in_shape)
+
+        h = Conv2D(32, kernel_size=(3, 3), activation='relu',
+                   input_shape=in_shape)(x)
+        h = Conv2D(64, (3, 3), activation='relu')(h)
+        h = MaxPooling2D(pool_size=(2, 2))(h)
+        h = Dropout(0.25)(h)
+        h = Flatten()(h)
+        
+        z_cl = h
+
+        h = Dense(128, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        
+        z_fl = h
+
+        y = Dense(nb_classes, activation='softmax', name='preds')(h)
+        
+        model.x, model.y = x, y # change line 57 to line 51 
+        super().__init__(model.x, model.y) # change __init__ to build_model
+        
+        model.cl_part = Model(x, z_cl) # Model(input, output)
+        model.fl_part = Model(x, z_fl)
+
+        #model.x, model.y = x, y
+
+    def compile(model):
+        Model.compile(model, loss='categorical_crossentropy',
+                      optimizer='adadelta', metrics=['accuracy'])
+
+
+class DataSet():
+    
+    def __init__(self, X, y, nb_classes, scaling=True, test_size=0.2, random_state=0):
+        """
+        Parameter 
+        X
+        y
+        nb_classes
+        """
+        
+        # assign as member var
+        self.X = X
+        
+        # add channel information
+        self.add_channels()
+        X = self.X
+        
+        # the data, shuffled and split between train and test sets
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(
+            X, y, test_size=0.2, random_state=random_state)
+
+        print(X_train.shape, y_train.shape)
+
+        X_train = X_train.astype('float32')
+        X_test = X_test.astype('float32')
+
+        if scaling:
+            # scaling to have (0, 1) for each feature (each pixel)
+            scaler = MinMaxScaler()
+            n = X_train.shape[0]
+            X_train = scaler.fit_transform(
+                X_train.reshape(n, -1)).reshape(X_train.shape)
+            n = X_test.shape[0]
+            X_test = scaler.transform(
+                X_test.reshape(n, -1)).reshape(X_test.shape)
+            self.scaler = scaler
+
+        print('X_train shape:', X_train.shape)
+        print(X_train.shape[0], 'train samples')
+        print(X_test.shape[0], 'test samples')
+
+        # convert class vectors to binary class matrices
+        Y_train = np_utils.to_categorical(y_train, nb_classes)
+        Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+        self.X_train, self.X_test = X_train, X_test
+        self.Y_train, self.Y_test = Y_train, Y_test
+        self.y_train, self.y_test = y_train, y_test
+        # self.input_shape = input_shape
+
+    def add_channels(self):
+        X = self.X
+        
+        
+        if len(X.shape) == 3: # if gray image
+            # N : batch
+            # img_rows : height
+            # img_cols : width
+            N, img_rows, img_cols = X.shape
+            
+            # check backend type 
+            if K.image_dim_ordering() == 'th': # if theano
+                # [N, height, width] -> [N, 1, height, width]
+                X = X.reshape(X.shape[0], 1, img_rows, img_cols)
+                input_shape = (1, img_rows, img_cols)
+
+            else:
+                X = X.reshape(X.shape[0], img_rows, img_cols, 1)
+                input_shape = (img_rows, img_cols, 1)
+        
+        else: # color image
+            # X.shape : [batch, height, width, channel]
+            input_shape = X.shape[1:]  # channel is already included.
+
+        self.X = X
+        self.input_shape = input_shape
+
+# Highest class
+class Machine():
+    def __init__(self, X, y, nb_classes=2, fig=True):
+        self.nb_classes = nb_classes
+        self.set_data(X, y) # Setting data
+        self.set_model() # Setting model
+        self.fig = fig
+
+    def set_data(self, X, y):
+        nb_classes = self.nb_classes
+        self.data = DataSet(X, y, nb_classes)
+        print('data.input_shape', self.data.input_shape)
+
+    def set_model(self):
+        nb_classes = self.nb_classes
+        data = self.data
+        self.model = CNN(nb_classes=nb_classes, in_shape=data.input_shape)
+        # cnn_lenet(nb_classes=nb_classes, in_shape=data.input_shape)
+
+    def fit(self, epochs=10, batch_size=128, verbose=1):
+        data = self.data
+        model = self.model
+
+        history = model.fit(data.X_train, data.Y_train, batch_size=batch_size, epochs=epochs,
+                            verbose=verbose, validation_data=(data.X_test, data.Y_test))
+        return history
+
+    def run(self, epochs=100, batch_size=128, verbose=1):
+        data = self.data
+        model = self.model
+        fig = self.fig
+
+        history = self.fit(epochs=epochs,
+                           batch_size=batch_size, verbose=verbose)
+
+        score = model.evaluate(data.X_test, data.Y_test, verbose=0)
+
+        print('Confusion matrix')
+        Y_test_pred = model.predict(data.X_test, verbose=0)
+        y_test_pred = np.argmax(Y_test_pred, axis=1)
+        print(metrics.confusion_matrix(data.y_test, y_test_pred))
+
+        print('Test score:', score[0])
+        print('Test accuracy:', score[1])
+
+        # Save results
+        suffix = sfile.unique_filename('datatime')
+        foldname = 'output_' + suffix
+        os.makedirs(foldname)
+        skeras.save_history_history(
+            'history_history.npy', history.history, fold=foldname)
+        model.save_weights(os.path.join(foldname, 'dl_model.h5'))
+        print('Output results are saved in', foldname)
+
+        if fig:
+            skeras.plot_acc_loss(history)
+
+        self.history = history
+
+        return foldname
---
+++ keras/3분_케라스/keraspp/skeras.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+import matplotlib
+
+def save_history_history(fname, history_history, fold=''):
+    np.save(os.path.join(fold, fname), history_history)
+
+
+def load_history_history(fname, fold=''):
+    history_history = np.load(os.path.join(fold, fname)).item(0)
+    return history_history
+
+
+def plot_acc(history, title=None):
+    # summarize history for accuracy
+    if not isinstance(history, dict):
+        history = history.history
+
+    plt.plot(history['acc'])
+    plt.plot(history['val_acc'])
+    if title is not None:
+        plt.title(title)
+    plt.ylabel('Accracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Training data', 'Validation data'], loc=0)
+    # plt.show()
+
+
+def plot_loss(history, title=None):
+    # summarize history for loss
+    if not isinstance(history, dict):
+        history = history.history
+
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    if title is not None:
+        plt.title(title)
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Training data', 'Validation data'], loc=0)
+    # plt.show()
+
+
+def plot_history(history):
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 2, 1)
+    plot_acc(history)
+    plt.subplot(1, 2, 2)
+    plot_loss(history)
+
+
+def plot_loss_acc(history):
+    plot_loss(history, '(a) Loss trajectory')
+    plt.show()
+    plot_acc(history, '(b) Accracy trajectory')
+    plt.show()
+
+
+def plot_acc_loss(history):
+    plot_acc(history, '(a) Accracy trajectory')
+    plt.show()
+    plot_loss(history, '(b) Loss trajectory')
+    plt.show()
