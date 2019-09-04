116bb414dd967d755a2bacc3035ce78d126781f8
kkkjerry <jaeyeol2931@gmail.com>
2019-09-04 13:48:58 +0900

    Update

--- tensorflow/keras/03_Advance/CNN/vgg.py
+++ tensorflow/keras/03_Advance/CNN/vgg.py
@@ -1,5 +1,12 @@
 from tensorflow.keras import layers, models, losses, optimizers
 
+def conv_block(input_layer, out_channel, num_iters, name=name+"_block1"):
+    prev_layer = input_layer
+    for i in range(num_iters):
+        output = layers.Conv2D(out_channel, 3, strides=1, padding='same', activation='relu', name=name+"_conv%d"%(i+1))(prev_layer)
+        prev_layer = output
+    return output
+
 def build_vgg(input_shape=(None, None, 3), num_layer=16, name='vgg'):
     num_layer_list = [11, 13, 16, 19]
     
@@ -12,20 +19,14 @@ def build_vgg(input_shape=(None, None, 3), num_layer=16, name='vgg'):
 
     num_channel_list = [64, 128, 256, 512, 512]
 
-    def conv_block(input_layer, out_channel, num_iters, name=name+"_block1"):
-        prev_layer = input_layer
-        for i in range(num_iters):
-            output = layers.Conv2D(out_channel, 3, strides=1, padding='same', activation='relu', name=name+"_conv%d"%(i+1))(prev_layer)
-            prev_layer = output
-        return output
-
     assert num_layer in  num_layer_list, "Number of layer must be in %s"%num_layer_list
     
-    input_layer = layers.Input(shape=input_shape, name=name+str(num_layer)+"_input")
+    name = name+str(num_layer)
+    input_layer = layers.Input(shape=input_shape, name=name+"_input")
     prev_layer = input_layer
     for idx, num_iter in enumerate(blocks_dict[num_layer]):
-        output = conv_block(prev_layer, num_channel_list[idx], num_iter, name=name+str(num_layer)+"_block%d"%(idx+1))
-        output = layers.MaxPool2D(name=name+str(num_layer)+"_block%d_pool"%(idx+1))(output)
+        output = conv_block(prev_layer, num_channel_list[idx], num_iter, name=name+"_block%d"%(idx+1))
+        output = layers.MaxPool2D(name=name+"_block%d_pool"%(idx+1))(output)
         prev_layer = output
     
-    return models.Model(inputs= input_layer, outputs=output, name=name+str(num_layer))
+    return models.Model(inputs= input_layer, outputs=output, name=name)
