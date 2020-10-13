3653c1428eb1530201e06104b75f8d241d0dbef7
jjerry-k <jaeyeol2931@gmail.com>
2020-10-14 02:54:08 +0900

    Ready for SR

---
+++ 04_Extra/Super_Resolution/download.py
@@ -0,0 +1,8 @@
+import os
+import cv2 as cv
+from tqdm import tqdm
+from tensorflow.keras import utils
+
+dataset_url = "http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/BSR/BSR_bsds500.tgz"
+data_dir = utils.get_file(origin=dataset_url, fname="BSR", untar=True)
+root_dir = os.path.join(data_dir, "BSDS500/data")
