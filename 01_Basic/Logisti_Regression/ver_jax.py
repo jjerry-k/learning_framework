36bbb4d12d30e5579d9cb18dc82719c109eddf9b
jjerry-k <jaeyeol2931@gmail.com>
2021-02-03 00:47:12 +0900

    Update

--- 01_Basic/Logisti_Regression/ver_jax.py
+++ 01_Basic/Logisti_Regression/ver_jax.py
@@ -1,9 +1,13 @@
+import sys
+sys.path.append('../../')
 import time
 import numpy as np
 from jax import jit, grad
 from jax.scipy.special import logsumexp
 import jax.numpy as jnp
+import jax.nn as nn
 from matplotlib import pyplot as plt
+from utils import jax_dataset
 
 def init_random_params(scale, layer_sizes, rng=np.random.RandomState(0)):
     return [(scale * rng.randn(m, n), scale * rng.randn(n))
@@ -11,42 +15,39 @@ def init_random_params(scale, layer_sizes, rng=np.random.RandomState(0)):
 
 def predict(params, inputs):
     outputs = jnp.dot(inputs, params[0][0]) + params[0][1]
-    return outputs
+    return nn.sigmoid(outputs)
 
 def loss(params, batch):
     inputs, targets = batch
     preds = predict(params, inputs)
-    return jnp.mean(jnp.sum((preds - targets)**2, axis=1))
-    # return -jnp.mean(jnp.sum(preds - targets, axis=1))
+    return -jnp.mean(jnp.log(preds) * targets)
     
 def accuracy(params, batch):
     inputs, targets = batch
-    target_class = jnp.argmax(targets, axis=1)
-    predicted_class = jnp.argmax(predict(params, inputs), axis=1)
-    return jnp.mean(predicted_class == target_class)
+    predicted_class = jnp.greater_equal(predict(params, inputs), 0.5)
+    return jnp.mean(predicted_class == targets)
 
-layer_sizes = [1, 1]
+layer_sizes = [784, 1]
 param_scale= 1
 step_size= 0.01
 num_epochs= 10
-batch_size= 256
+batch_size= 64
 
-x = np.random.normal(0.0, 0.55, (10000, 1))
-y = x * 0.1 + 0.3 + np.random.normal(0.0, 0.03, (10000,1))
-plt.plot(x, y, 'r.')
-plt.show()
+train_images, train_labels, test_images, test_labels = jax_dataset.mnist()
+train_labels= np.greater_equal(np.argmax(train_labels, axis=1)[..., np.newaxis], 0.5).astype(np.float32)
+test_labels= np.greater_equal(np.argmax(test_labels, axis=1)[..., np.newaxis], 0.5).astype(np.float32)
 
-num_train = x.shape[0]
+num_train = train_images.shape[0]
 num_complete_batches, leftover = divmod(num_train, batch_size)
 num_batches = num_complete_batches + bool(leftover)
-
+# %%
 def data_stream():
     rng = np.random.RandomState(0)
     while True:
         perm = rng.permutation(num_train)
         for i in range(num_batches):
             batch_idx = perm[i * batch_size:(i + 1) * batch_size]
-            yield x[batch_idx], y[batch_idx]
+            yield train_images[batch_idx], train_labels[batch_idx]
 batches = data_stream()
 
 @jit
@@ -58,13 +59,15 @@ def update(params, batch):
 params = init_random_params(param_scale, layer_sizes)
 for epoch in range(num_epochs):
     start_time = time.time()
+    loss_val = 0
     for _ in range(num_batches):
         batch_data = next(batches)
+        loss_val += loss(params, batch_data)
         params = update(params, batch_data)
     epoch_time = time.time() - start_time
-    y_ = x*params[0][0] + params[0][1]
-    if (epoch + 1)%2 == 0:
-        plt.plot(x, y, 'r.')
-        plt.plot(x, y_, 'b-')
-        plt.show()
-    print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+
+    train_acc = accuracy(params, (train_images, train_labels))
+    test_acc = accuracy(params, (test_images, test_labels))
+    print(f"Epoch: {epoch+1}, Loss: {loss_val/num_batches}, Elapsed time: {epoch_time:0.2f} sec")
+    print("Training set accuracy {}".format(train_acc))
+    print("Test set accuracy {}".format(test_acc))
