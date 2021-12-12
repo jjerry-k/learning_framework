c4079fb67a237e8d02b5ad4da0712d2769b6e1d2
jjerry-k <jaeyeol2931@gmail.com>
2021-12-12 23:34:35 +0900

    Update simple rnn

---
+++ 02_Intermediate/Simple_Recurrent_Neural_Network/PyTorch.py
@@ -0,0 +1,114 @@
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn.functional as F
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader 
+
+from torchvision import transforms, datasets, utils
+
+# Device Configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# MNIST dataset
+mnist_train = datasets.MNIST(root='../../data/',
+                          train=True,
+                          transform=transforms.ToTensor(),
+                          download=True)
+print("Downloading Train Data Done ! ")
+
+mnist_test = datasets.MNIST(root='../../data/',
+                         train=False,
+                         transform=transforms.ToTensor(),
+                         download=True)
+print("Downloading Test Data Done ! ")
+
+# our model
+class Model(nn.Module):
+    
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(Model, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        pass
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+
+        out, hidden = self.rnn(x, h0)  
+        
+        out = self.fc(out[:, -1, :])
+        return out
+
+model = Model(28, 128, 2, 10).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 10
+batch_size = 256
+
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=2)
+val_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=2)
+
+
+print("Iteration maker Done !")
+
+# Training
+for epoch in range(epochs):
+    model.train()
+    avg_loss = 0
+    avg_acc = 0
+    
+    with tqdm(total=len(train_loader)) as t:
+        t.set_description(f'[{epoch+1}/{epochs}]')
+        total = 0
+        correct = 0
+        for i, (batch_img, batch_lab) in enumerate(train_loader):
+            X = batch_img.squeeze().to(device) # B, 1, 28, 28 -> B, 28, 28
+            Y = batch_lab.to(device)
+
+            optimizer.zero_grad()
+
+            y_pred = model.forward(X)
+
+            loss = criterion(y_pred, Y)
+            
+            loss.backward()
+            optimizer.step()
+            avg_loss += loss.item()
+
+            _, predicted = torch.max(y_pred.data, 1)
+            total += Y.size(0)
+            correct += (predicted == Y).sum().item()
+            
+            t.set_postfix({"loss": f"{loss.item():05.3f}"})
+            t.update()
+        acc = (100 * correct / total)
+
+    model.eval()
+    with tqdm(total=len(val_loader)) as t:
+        t.set_description(f'[{epoch+1}/{epochs}]')
+        with torch.no_grad():
+            val_loss = 0
+            total = 0
+            correct = 0
+            for i, (batch_img, batch_lab) in enumerate(val_loader):
+                X = batch_img.squeeze().to(device)
+                Y = batch_lab.to(device)
+                y_pred = model(X)
+                val_loss += criterion(y_pred, Y)
+                _, predicted = torch.max(y_pred.data, 1)
+                total += Y.size(0)
+                correct += (predicted == Y).sum().item()
+                t.set_postfix({"val_loss": f"{val_loss.item()/(i+1):05.3f}"})
+                t.update()
+
+            val_loss /= total
+            val_acc = (100 * correct / total)
+            
+    print(f"Epoch : {epoch+1}, Loss : {(avg_loss/len(train_loader)):.3f}, Acc: {acc:.3f}, Val Loss : {val_loss.item():.3f}, Val Acc : {val_acc:.3f}\n")
+
+print("Training Done !")
