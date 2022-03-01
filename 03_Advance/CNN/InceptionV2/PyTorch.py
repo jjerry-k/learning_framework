26af38acac46c4cc7842a70f8b0f8aaadb89d7e8
jjerry-k <jaeyeol2931@gmail.com>
2022-03-02 00:24:36 +0900

    Update InceptionV2

--- 03_Advance/CNN/InceptionV2/PyTorch.py
+++ 03_Advance/CNN/InceptionV2/PyTorch.py
@@ -1,104 +1,111 @@
-# %%
+# Importing Modules
 import os
+import random
 from tqdm import tqdm
 
-import cv2 as cv
 import numpy as np
+from PIL import Image
 
 import torch
-from torch import nn, optim
-from torch.nn import functional as F
+from torch import nn
+from torch import optim
 from torch.utils.data import Dataset, DataLoader 
-from torchvision import transforms, datasets, utils
 
+from torchvision import transforms
+
+from matplotlib import pyplot as plt
 
 # Device Configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# %%
-SAVE_PATH = "../../../data"
-URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-file_name = URL.split("/")[-1]
-data = datasets.utils.download_and_extract_archive(URL, SAVE_PATH)
-PATH = os.path.join(SAVE_PATH, "flower_photos")
+# Set randomness
+seed = 777
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
 
-category_list = [i for i in os.listdir(PATH) if os.path.isdir(os.path.join(PATH, i)) ]
-print(category_list)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
-num_classes = len(category_list)
-img_size = 128
+# Set hyperparameter
+epochs= 5
+batch_size= 16
+img_size= 192
 
-def read_img(path, img_size):
-    img = cv.imread(path)
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-    img = cv.resize(img, (img_size, img_size))
-    return img
+# Dataset
+class FlowerDataset(Dataset):
+    def __init__(self, data_dir, transform):
+        IMG_FORMAT = ["jpg", "jpeg", "bmp", "png", "tif", "tiff"]
+        self.filelist = []
+        self.classes = sorted(os.listdir(data_dir))
+        for root, _, files in os.walk(data_dir):
+            if not len(files): continue
+            files = [os.path.join(root, file) for file in files if file.split(".")[-1] in IMG_FORMAT]
+            self.filelist += files
+        # self.filelist = self.filelist[:64]
+        self.transform = transform
 
-imgs_tr = []
-labs_tr = []
+    def __len__(self):
+        # return size of dataset
+        return len(self.filelist)
 
-imgs_val = []
-labs_val = []
+    def __getitem__(self, idx):
 
-for i, category in enumerate(category_list):
-    path = os.path.join(PATH, category)
-    imgs_list = os.listdir(path)
-    print("Total '%s' images : %d"%(category, len(imgs_list)))
-    ratio = int(np.round(0.05 * len(imgs_list)))
-    print("%s Images for Training : %d"%(category, len(imgs_list[ratio:])))
-    print("%s Images for Validation : %d"%(category, len(imgs_list[:ratio])))
-    print("=============================")
+        image = Image.open(self.filelist[idx]).convert("RGB")
+        image = self.transform(image)
+        label = self.filelist[idx].split('/')[-2]
+        label = self.classes.index(label)
+        return image, label
 
-    imgs = [read_img(os.path.join(path, img),img_size) for img in imgs_list]
-    labs = [i]*len(imgs_list)
+transform = transforms.Compose([
+                                transforms.Resize((img_size, img_size)), transforms.ToTensor()
+                                ])
+train_dataset = FlowerDataset(os.path.join("../../../data/flower_photos/train"), transform)
+val_dataset = FlowerDataset(os.path.join("../../../data/flower_photos/validation"), transform)
 
-    imgs_tr += imgs[ratio:]
-    labs_tr += labs[ratio:]
-    
-    imgs_val += imgs[:ratio]
-    labs_val += labs[:ratio]
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
-imgs_tr = np.array(imgs_tr)/255.
-labs_tr = np.array(labs_tr)
-
-imgs_val = np.array(imgs_val)/255.
-labs_val = np.array(labs_val)
-
-print(imgs_tr.shape, labs_tr.shape)
-print(imgs_val.shape, labs_val.shape)
-
-# %%
-# Build network
-
-class Inception_Module(nn.Module):
+# Defining Model
+class InceptionModule(nn.Module):
     def __init__(self, input_feature, 
                 filters_b1, filters_b2_1, filters_b2_2, 
                 filters_b3_1, filters_b3_2, filters_b3_3, filters_b4):
-        super(Inception_Module, self).__init__()
+        super(InceptionModule, self).__init__()
 
         self.branch1 = nn.Sequential(
             nn.Conv2d(input_feature, filters_b1, 1), 
+            nn.BatchNorm2d(filters_b1),
             nn.ReLU(True))
 
         self.branch2 = nn.Sequential(
             nn.Conv2d(input_feature, filters_b2_1, 1),
+            nn.BatchNorm2d(filters_b2_1),
             nn.ReLU(True),
             nn.Conv2d(filters_b2_1, filters_b2_2, 3, 1, 1),
+            nn.BatchNorm2d(filters_b2_2),
             nn.ReLU(True)
         )
 
         self.branch3 = nn.Sequential(
             nn.Conv2d(input_feature, filters_b3_1, 1),
+            nn.BatchNorm2d(filters_b3_1),
             nn.ReLU(True),
             nn.Conv2d(filters_b3_1, filters_b3_2, 3, 1, 1),
+            nn.BatchNorm2d(filters_b3_2),
             nn.ReLU(True),
             nn.Conv2d(filters_b3_2, filters_b3_3, 3, 1, 1),
+            nn.BatchNorm2d(filters_b3_3),
             nn.ReLU(True)
         )
 
         self.branch4 = nn.Sequential(
             nn.AvgPool2d(3, 1, 1),
             nn.Conv2d(input_feature, filters_b4, 1),
+            nn.BatchNorm2d(filters_b4),
             nn.ReLU(True)
         )
 
@@ -106,24 +113,29 @@ class Inception_Module(nn.Module):
         return torch.cat([self.branch1(x), self.branch2(x), self.branch3(x), self.branch4(x)], dim=1)
 
 
-class Grid_Reduction(nn.Module):
+class GridReduction(nn.Module):
     def __init__(self, input_feature, filters_b1_1, filters_b1_2, 
                 filters_b2_1, filters_b2_2, filters_b2_3):
-        super(Grid_Reduction, self).__init__()
+        super(GridReduction, self).__init__()
 
         self.branch1 = nn.Sequential(
             nn.Conv2d(input_feature, filters_b1_1, 1),
+            nn.BatchNorm2d(filters_b1_1),
             nn.ReLU(True),
             nn.Conv2d(filters_b1_1, filters_b1_2, 3, 2),
+            nn.BatchNorm2d(filters_b1_2),
             nn.ReLU(True)
         )
 
         self.branch2 = nn.Sequential(
             nn.Conv2d(input_feature, filters_b2_1, 1),
+            nn.BatchNorm2d(filters_b2_1),
             nn.ReLU(True),
             nn.Conv2d(filters_b2_1, filters_b2_2, 3, 1, 1),
+            nn.BatchNorm2d(filters_b2_2),
             nn.ReLU(True),
             nn.Conv2d(filters_b2_2, filters_b2_3, 3, 2),
+            nn.BatchNorm2d(filters_b2_3),
             nn.ReLU(True)
         )
 
@@ -133,12 +145,13 @@ class Grid_Reduction(nn.Module):
     def forward(self, x):
         return torch.cat([self.branch1(x), self.branch2(x), self.branch3(x)], dim=1)
 
-class Auxiliary_Classifier(nn.Module):
+class AuxiliaryClassifier(nn.Module):
     def __init__(self, input_feature, num_classes):
-        super(Auxiliary_Classifier, self).__init__()
+        super(AuxiliaryClassifier, self).__init__()
         self.block = nn.Sequential(
             nn.AvgPool2d(5, 3, 1),
             nn.Conv2d(input_feature, 128, 1),
+            nn.BatchNorm2d(128),
             nn.ReLU(True),
             nn.AdaptiveAvgPool2d((1,1)),
             nn.Flatten(),
@@ -149,35 +162,38 @@ class Auxiliary_Classifier(nn.Module):
         return self.block(x)
 
         
-class Build_InceptionV2(nn.Module):
+class InceptionV2(nn.Module):
     def __init__(self, input_channel=3, num_classes=1000):
-        super(Build_InceptionV2, self).__init__()
+        super(InceptionV2, self).__init__()
 
         self.Stem = nn.Sequential(
             nn.ZeroPad2d(3),
-            nn.Conv2d(3, 64, 7, 2),
+            nn.Conv2d(input_channel, 64, 7, 2),
+            nn.BatchNorm2d(64),
             nn.ReLU(True),
             nn.MaxPool2d(3, 2),
             
             nn.Conv2d(64, 64, 1),
+            nn.BatchNorm2d(64),
             nn.ReLU(True),
             nn.Conv2d(64, 192, 3, 1, 1),
+            nn.BatchNorm2d(192),
             nn.ReLU(True),
             nn.MaxPool2d(3, 2)
         )
         
-        self.inception1 = Inception_Module(192, 64, 64, 64, 64, 96, 96, 32)
-        self.inception2 = Inception_Module(256, 64, 64, 96, 64, 96, 96, 64)
-        self.grid_reduction1 = Grid_Reduction(320, 128, 160, 64, 96, 96)
+        self.inception1 = InceptionModule(192, 64, 64, 64, 64, 96, 96, 32)
+        self.inception2 = InceptionModule(256, 64, 64, 96, 64, 96, 96, 64)
+        self.grid_reduction1 = GridReduction(320, 128, 160, 64, 96, 96)
 
-        self.inception3 = Inception_Module(576, 224, 64, 96, 96, 128, 128, 128)
-        self.inception4 = Inception_Module(576, 224, 64, 96, 96, 128, 128, 128)
-        self.inception5 = Inception_Module(576, 160, 128, 160, 128, 160, 160, 96)
-        self.inception6 = Inception_Module(576, 96, 128, 192, 160, 192, 192, 96)
-        self.grid_reduction2 = Grid_Reduction(576, 128, 192, 192, 256, 256)
+        self.inception3 = InceptionModule(576, 224, 64, 96, 96, 128, 128, 128)
+        self.inception4 = InceptionModule(576, 224, 64, 96, 96, 128, 128, 128)
+        self.inception5 = InceptionModule(576, 160, 128, 160, 128, 160, 160, 96)
+        self.inception6 = InceptionModule(576, 96, 128, 192, 160, 192, 192, 96)
+        self.grid_reduction2 = GridReduction(576, 128, 192, 192, 256, 256)
 
-        self.inception7 = Inception_Module(1024, 352, 192, 160, 224, 224, 224, 128)
-        self.inception8 = Inception_Module(1024, 352, 192, 320, 192, 224, 224, 128)
+        self.inception7 = InceptionModule(1024, 352, 192, 160, 224, 224, 224, 128)
+        self.inception8 = InceptionModule(864, 352, 192, 320, 192, 224, 224, 128)
 
         self.Classifier = nn.Sequential(
             nn.AdaptiveAvgPool2d((1,1)),
@@ -186,23 +202,17 @@ class Build_InceptionV2(nn.Module):
             nn.Linear(1024, num_classes)
         )
 
-        self.init_weights(self.Stem)
-        self.init_weights(self.inception1)
-        self.init_weights(self.inception2)
-        self.init_weights(self.grid_reduction1)
-        self.init_weights(self.inception3)
-        self.init_weights(self.inception4)
-        self.init_weights(self.inception5)
-        self.init_weights(self.inception6)
-        self.init_weights(self.grid_reduction2)
-        self.init_weights(self.inception7)
-        self.init_weights(self.inception8)
-        self.init_weights(self.Classifier)
+        self.init_weights()
 
-    def init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform_(m.weight)
-            m.bias.data.fill_(0.01)
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
         
     def forward(self, x):
         x = self.Stem(x)
@@ -221,39 +231,13 @@ class Build_InceptionV2(nn.Module):
         x = self.Classifier(x)
         return x
 
-net = Build_InceptionV2(input_channel=imgs_tr.shape[-1], num_classes=5).to(device)
+model = InceptionV2(input_channel=3, num_classes=5).to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.0001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# %%
-epochs=100
-batch_size=16
-
-class CustomDataset(Dataset):
-    def __init__(self, train_x, train_y): 
-        self.len = len(train_x) 
-        self.x_data = torch.tensor(np.transpose(train_x, [0, 3, 1, 2]), dtype=torch.float)
-        self.y_data = torch.tensor(train_y, dtype=torch.long) 
-
-    def __getitem__(self, index): 
-        return self.x_data[index], self.y_data[index] 
-
-    def __len__(self): 
-        return self.len
-        
-train_dataset = CustomDataset(imgs_tr, labs_tr) 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-
-val_dataset = CustomDataset(imgs_val, labs_val) 
-val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-
-print("Iteration maker Done !")
-
-# %%
-# Training Network
-
+# Training
 for epoch in range(epochs):
-    net.train()
+    model.train()
     avg_loss = 0
     avg_acc = 0
     
@@ -265,12 +249,11 @@ for epoch in range(epochs):
             X = batch_img.to(device)
             Y = batch_lab.to(device)
 
-            optimizer.zero_grad()
-
-            y_pred = net.forward(X)
+            y_pred = model.forward(X)
 
             loss = criterion(y_pred, Y)
             
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             avg_loss += loss.item()
@@ -279,11 +262,11 @@ for epoch in range(epochs):
             total += Y.size(0)
             correct += (predicted == Y).sum().item()
             
-            t.set_postfix({"loss": f"{loss.item():05.3f}"})
+            t.set_postfix({"loss": f"{avg_loss/(i+1):05.3f}"})
             t.update()
         acc = (100 * correct / total)
 
-    net.eval()
+    model.eval()
     with tqdm(total=len(val_loader)) as t:
         t.set_description(f'[{epoch+1}/{epochs}]')
         with torch.no_grad():
@@ -293,7 +276,7 @@ for epoch in range(epochs):
             for i, (batch_img, batch_lab) in enumerate(val_loader):
                 X = batch_img.to(device)
                 Y = batch_lab.to(device)
-                y_pred = net(X)
+                y_pred = model(X)
                 val_loss += criterion(y_pred, Y)
                 _, predicted = torch.max(y_pred.data, 1)
                 total += Y.size(0)
@@ -301,9 +284,9 @@ for epoch in range(epochs):
                 t.set_postfix({"val_loss": f"{val_loss.item()/(i+1):05.3f}"})
                 t.update()
 
-            val_loss /= total
+            val_loss /= len(val_loader)
             val_acc = (100 * correct / total)
             
-    print(f"Epoch : {epoch+1}, Loss : {(avg_loss/len(train_loader)):.3f}, Acc: {acc:.3f}, Val Loss : {val_loss.item():.3f}, Val Acc : {val_acc:.3f}")
+    print(f"Epoch : {epoch+1}, Loss : {(avg_loss/len(train_loader)):.3f}, Acc: {acc:.3f}, Val Loss : {val_loss.item():.3f}, Val Acc : {val_acc:.3f}\n")
 
 print("Training Done !")
