6a9e7e8c35792a968009d0c8e291cb288b4b32bc
jjerry-k <jaeyeol2931@gmail.com>
2022-03-30 23:21:14 +0900

    Update Convolutional Auto Encoder

--- 03_Advance/AutoEncoder/CAE/PyTorch.py
+++ 03_Advance/AutoEncoder/CAE/PyTorch.py
@@ -1,35 +1,58 @@
-import os
+# Importing Modules
+import random
 from tqdm import tqdm
 
-import cv2 as cv
 import numpy as np
 
 import torch
-from torch import nn, optim
-from torch.nn import functional as F
-from torch.utils.data import Dataset, DataLoader 
-from torchvision import transforms, datasets, utils
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader 
+
+from torchvision import datasets
+from torchvision import transforms
+
+from matplotlib import pyplot as plt
 
 # Device Configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+# Set randomness
+seed = 777
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+# Set hyperparameter
+epochs= 10
+batch_size= 256
+
 # MNIST dataset
-train_dataset = datasets.MNIST(root='../../../data/',
-                          train=True,
-                          transform=transforms.ToTensor(),
-                          download=True)
+mnist_train = datasets.MNIST(root='../../../data/',
+                            train=True,
+                            transform=transforms.ToTensor(),
+                            download=True)
 print("Downloading Train Data Done ! ")
 
-val_dataset = datasets.MNIST(root='../../../data/',
-                         train=False,
-                         transform=transforms.ToTensor(),
-                         download=True)
+mnist_test = datasets.MNIST(root='../../../data/',
+                            train=False,
+                            transform=transforms.ToTensor(),
+                            download=True)
 print("Downloading Test Data Done ! ")
 
-# Build network
-class build_CAE(nn.Module):
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=2)
+val_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=2)
+
+# Defining Model
+class BuildCAE(nn.Module):
     def __init__(self, input_features=1):
-        super(build_CAE, self).__init__()
+        super(BuildCAE, self).__init__()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(input_features, 16, 3, 1, 1),
@@ -60,22 +83,12 @@ class build_CAE(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
 
-net = build_CAE(input_features=1).to(device)
+model = BuildCAE(input_features=1).to(device)
 criterion = nn.MSELoss()
-optimizer = optim.Adam(net.parameters(), lr = 0.0001)
-
-epochs=10
-batch_size=256
-
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, num_workers=2)
-
-print("Iteration maker Done !")
-
-# Training Network
+optimizer = optim.Adam(model.parameters(), lr = 0.0001)
 
 for epoch in range(epochs):
-    net.train()
+    model.train()
     avg_loss = 0
     
     with tqdm(total=len(train_loader)) as t:
@@ -85,7 +98,7 @@ for epoch in range(epochs):
             X = batch_img.to(device)
             
             optimizer.zero_grad()
-            y_pred = net.forward(X)
+            y_pred = model.forward(X)
             loss = criterion(y_pred, X)
             
             loss.backward()
@@ -95,7 +108,7 @@ for epoch in range(epochs):
             t.set_postfix({"loss": f"{loss.item():05.3f}"})
             t.update()
 
-    net.eval()
+    model.eval()
     with tqdm(total=len(val_loader)) as t:
         t.set_description(f'[{epoch+1}/{epochs}]')
         with torch.no_grad():
@@ -104,7 +117,7 @@ for epoch in range(epochs):
                 
                 X = batch_img.to(device)
                 
-                y_pred = net(X)
+                y_pred = model(X)
                 val_loss += criterion(y_pred, X)
                 t.set_postfix({"val_loss": f"{val_loss.item()/(i+1):05.3f}"})
                 t.update()
